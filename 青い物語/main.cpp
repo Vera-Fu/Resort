d01192d9de435ec6142fa60cb670880ae4aea10e
Vera_FU <851537946@qq.com>
2021-02-02 18:19:03 +0900

    什么都没有的版本，只有老师给的“引擎”

---
+++ 青い物語/main.cpp
@@ -0,0 +1,108 @@
+﻿//=================================================
+// [アクションサンプル]
+// main.cpp
+// author:**** ****	Date:2021/**/**
+//=================================================
+#include "main.h"
+//#include "player.h"
+////#include "map.h"
+#include "conioex.h"
+//#include "enemy.h"
+//#include "bullet.h"
+
+void Init(void);
+void Uninit(void);
+void Update(void);
+void Draw(void);
+void DispFPS(void);
+
+int g_nCountFPS;				// FPSカウンタ
+
+
+
+void main(void)
+{
+	setcursortype(NOCURSOR);;
+	DWORD dwExecLastTime;
+	DWORD dwFPSLastTime;
+	DWORD dwCurrentTime;
+	int nCountFrame;
+
+	// 分解能を設定
+	timeBeginPeriod(1);
+
+	dwExecLastTime =
+		dwFPSLastTime = timeGetTime();
+	dwCurrentTime =
+		nCountFrame = 0;
+
+	//初期化関数
+	Init();
+
+	do
+	{
+		dwCurrentTime = timeGetTime();
+		if ((dwCurrentTime - dwFPSLastTime) >= 500)	// 0.5秒ごとに実行
+		{
+			g_nCountFPS = nCountFrame * 1000 / (dwCurrentTime - dwFPSLastTime);
+			dwFPSLastTime = dwCurrentTime;
+			nCountFrame = 0;
+		}
+
+		if ((dwCurrentTime - dwExecLastTime) >= (1000 / 60))
+		{
+			dwExecLastTime = dwCurrentTime;
+
+			Update();
+
+#ifdef _DEBUG
+			DispFPS();
+#endif // _DEBUG
+
+			Draw();
+
+			nCountFrame++;
+		}
+	} while (!inport(PK_ESC));
+
+	Uninit();
+
+	// 分解能を戻す
+	timeEndPeriod(1);
+}
+
+void Init(void)
+{
+
+}
+
+void Uninit(void)
+{
+
+}
+
+void Update(void)
+{
+
+}
+
+void Draw(void)
+{
+
+}
+
+#ifdef _DEBUG
+
+//デバッグ表示
+void DispFPS(void) {
+
+	// 色設定
+	textcolor; LIGHTCYAN;
+
+	gotoxy(1, 1);	//表示位置設定
+	printf("FPS:%d", g_nCountFPS);
+
+	//　色設定もとに戻す
+	textcolor; WHITE;
+}
+#endif // _DEBUG
---
+++ 青い物語/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_	// £≤÷ÿ•§•Û•Ø•Î©`•…∑¿÷π§Œ•ﬁ•Ø•Ì∂®¡x
+#define _MAIN_H_
+
+#include <stdio.h>
+
+typedef struct {
+	float x;
+	float y;
+}FLOAT2;
+
+#endif
